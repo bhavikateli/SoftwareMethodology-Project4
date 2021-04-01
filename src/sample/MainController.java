ea85e0553afa7e56a5805d428b7d90bc1e823e32
bhavikateli <teli.bhavika@gmail.com>
2021-04-01 12:55:20 -0400

    Initial Commit

---
+++ src/sample/MainController.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainController {
+
+    @FXML
+    private Button orderDonutsButton;
+
+    @FXML
+    private Button orderCoffeeButton;
+
+    @FXML
+    private Button yourOrderButton;
+
+    @FXML
+    private Button storeOrderButton;
+
+}
